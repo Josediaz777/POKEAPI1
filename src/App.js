ca62229b7d4af1a8169650e485e68b42cfe7fae5
Josediaz777 <manueltiscareno1200@gmail.com>
2022-06-22 18:22:05 -0500

    feat:We make the functions to can get the pokemons ten in ten

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [position, setPosition] = useState(0);
+  const [infoPokemon, setInfoPokemon] = useState([]);
+  const [result, setResult] = useState([]);
+  const modalNombre = useRef(null);
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${position}&limit=10`).then(info => {
+      setInfoPokemon(info.data.results);
+    });
+  }, [position]);
+
+  useEffect(() => {
+    infoPokemon.map(data => {
+      const id = data.url.split('/');
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id[6]}`).then(data => {
+        setResult((current) => [...current, data]);
+      });
+    });
+  }, [infoPokemon]);
+
+  useEffect(() => {
+    setResult([]);
+    infoPokemon.map(data => {
+      const id = data.url.split('/');
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id[6]}`).then(data => {
+        setResult((current) => [...current, data]);
+      });
+    });
+  }, [position]);
+ 
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='titulo'>POKEDEX DE MANUEL</h1>
+      <hr></hr>
+      <button className='btn btn-outline-success' onClick={() => {setPosition(position - 10); console.log(modalNombre.current.alt);}}>Back</button>
+      <button className='btn btn-outline-success' onClick={() => {setPosition(position + 10); console.log(modalNombre.current.alt);}}>Next</button>
+      <div className='grid'>
+        {
+          result.length > 0 ? result.slice(0, 10).map((data, index) => (    
+            <div key={index} className="card">
+              <div className="container">                
+                <h4><b>{data.data.name}</b></h4> 
+                <img ref={modalNombre} src={data.data.sprites.front_default} alt={data.data.name}/>
+              </div>
+            </div>
+          )): null
+        }
+      </div>
     </div>
   );
 }
