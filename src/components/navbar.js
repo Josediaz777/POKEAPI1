3027dd77409c0645529c967f8118e321fee39344
Josediaz777 <manueltiscareno1200@gmail.com>
2022-07-14 16:49:50 -0500

    feat: we aggregate the navbar to the page for can navigate

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="topnav" id="myTopnav">
             <a href="/" className="active">Pokemons</a>
-            <a href="/191382">Acerca de </a>
+            <a href="/191382">Acerca de</a>
             <a className="icon">
                 <i className="fa fa-bars"></i>
             </a>
