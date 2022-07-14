30947eeb724f2ce9704020a5cbdc4e7c35d28f86
Josediaz777 <manueltiscareno1200@gmail.com>
2022-07-14 16:29:34 -0500

    Style: We aggegate the styles to the footer

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Acercade from './components/acercade';
+import Pokemon from './components/pokemons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-    </Routes>
-  </BrowserRouter>
+  <div>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/191382" element={<Acercade />} />
+        <Route path="/pokemons/:pokemonId" element={<Pokemon />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
